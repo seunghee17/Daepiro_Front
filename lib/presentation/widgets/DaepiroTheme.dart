37da987fb9f9e15b5bac9afd52b5b2bb97ca596a
seunghee17 <victoria9981@swu.ac.kr>
2024-08-31 13:03:20 +0900

    feat: 폰트 추가 및 컬러 추가

---
+++ lib/presentation/widgets/DaepiroTheme.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+class DaepiroColorStyle {
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color g_50 = Color(0xFFF5F5F7);
+  static const Color g_75 = Color(0xFFE4E5EA);
+  static const Color g_100 = Color(0xFFC9CCD4);
+  static const Color g_200 = Color(0xFFAFB3BF);
+  static const Color g_300 = Color(0xFF8B90A1);
+  static const Color g_400 = Color(0xFF757A8F);
+  static const Color g_500 = Color(0xFF525973);
+  static const Color g_600 = Color(0xFF4B5169);
+  static const Color g_700 = Color(0xFF3A3F52);
+  static const Color g_800 = Color(0xFF2D313F);
+  static const Color g_900 = Color(0xFF222530);
+
+  static const Color o_50 = Color(0xFFfff0ea);
+  static const Color o_100 = Color(0xFFffd1bd);
+  static const Color o_200 = Color(0xFFffba9d);
+  static const Color o_300 = Color(0xFFFF9B70);
+  static const Color o_400 = Color(0xFFFF8754);
+  static const Color o_500 = Color(0xFFFF6929);
+  static const Color o_600 = Color(0xFFE86025);
+  static const Color o_700 = Color(0xFFB54B1D);
+  static const Color o_800 = Color(0xFF8c3a17);
+  static const Color o_900 = Color(0xFF6B2C11);
+
+  static const Color gre_50 = Color(0xFFE9F8F0);
+  static const Color gre_100 = Color(0xFFBAE9D0);
+  static const Color gre_200 = Color(0xFF98DEB9);
+  static const Color gre_300 = Color(0xFF6ACF98);
+  static const Color gre_400 = Color(0xFF4DC585);
+  static const Color gre_500 = Color(0xFF20B766);
+  static const Color gre_600 = Color(0xFF1DA75D);
+  static const Color gre_700 = Color(0xFF178248);
+  static const Color gre_800 = Color(0xFF126538);
+  static const Color gre_900 = Color(0xFF0D4D2B);
+
+  static const Color r_50 = Color(0xFFFFEAE9);
+  static const Color r_100 = Color(0xFFFDBEBA);
+  static const Color r_200 = Color(0xFFFD9E98);
+  static const Color r_300 = Color(0xFFFC7269);
+  static const Color r_400 = Color(0xFFFB564C);
+  static const Color r_500 = Color(0xFFFA2C1F);
+  static const Color r_600 = Color(0xFFE4281C);
+  static const Color r_700 = Color(0xFFB21F16);
+  static const Color r_800 = Color(0xFF8A1811);
+  static const Color r_900 = Color(0xFF69120D);
+
+}
+
+class DaepiroTextStyle {
+  static final TextStyle h1 = TextStyle(
+    fontSize: 96,
+    fontWeight: FontWeight.w400,
+    height: 115 % 96,
+    letterSpacing: -1.5
+  );
+  static final h2 = TextStyle(
+      fontSize: 60,
+      fontWeight: FontWeight.w400,
+      height: 72 % 60,
+      letterSpacing: -0.5
+  );
+  static final h3 = TextStyle(
+      fontSize: 48,
+      fontWeight: FontWeight.w500,
+      height: 58 % 48,
+      letterSpacing: 0
+  );
+  static final h4 = TextStyle(
+      fontSize: 34,
+      fontWeight: FontWeight.w600,
+      height: 42 % 34,
+      letterSpacing: 0
+  );
+  static final h5 = TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w700,
+      height: 32 % 24,
+      letterSpacing: 0
+  );
+  static final h6 = TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      height: 28 % 20,
+      letterSpacing: 0.1
+  );
+  static final body_1_b = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w700,
+      height: 1.5,
+      letterSpacing: 0.15
+  );
+  static final body_1_m = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      height: 1.5,
+      letterSpacing: 0.15
+  );
+  static final body_2_b = TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      height: 22 % 14,
+      letterSpacing: -0.25
+  );
+  static final body_2_m = TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      height: 22 % 14,
+      letterSpacing: -0.25
+  );
+  static final caption = TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+      height: 16 % 12,
+      letterSpacing: 0
+  );
+  static final overline = TextStyle(
+      fontSize: 10,
+      fontWeight: FontWeight.w500,
+      height: 12 % 10,
+      letterSpacing: 0
+  );
+}
