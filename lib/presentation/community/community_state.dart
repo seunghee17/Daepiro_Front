5e3b26dbf4763d279f9b5de7df14ac6e150de612
seunghee17 <victoria9981@swu.ac.kr>
2024-10-20 01:01:02 +0900

    feat: ios 권한 처리 완료

--- lib/presentation/community/community_state.dart
+++ lib/presentation/community/community_state.dart
@@ -1,10 +1,10 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-// part 'community_state.freezed.dart';
-//
-// @freezed
-// sealed class CommunityState with _$CommunityState {
-//   factory CommunityState({
-//
-// }) = _CommunityState;
-// }
+part 'community_state.freezed.dart';
+
+@freezed
+sealed class CommunityState with _$CommunityState {
+  factory CommunityState({
+    @Default(false) bool isError,
+}) = _CommunityState;
+}
