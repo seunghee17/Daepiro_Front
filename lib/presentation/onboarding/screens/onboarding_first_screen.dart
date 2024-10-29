2c7c1686bb59a57e51e1af6d8af7aaf971b66041
macrent <victoria9981@fasoo.com>
2024-10-29 17:45:44 +0900

    feat: 위젯 영역 나눔

--- lib/presentation/onboarding/screens/onboarding_first_screen.dart
+++ lib/presentation/onboarding/screens/onboarding_first_screen.dart
@@ -10,7 +10,6 @@ class OnboardingFirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var screenHeight = MediaQuery.of(context).size.height; //812
-
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -18,31 +17,11 @@ class OnboardingFirstScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              //TODO: 여기 수정해야함
              SizedBox(height: screenHeight * 0.059,),
-              Text(
-                '반복되는 수신과 불명확한 재난문자로\n불편함을 겪으셨나요?',
-                style: DaepiroTextStyle.body_1_m.copyWith(color: DaepiroColorStyle.g_500),
-              ),
-              SizedBox(height: 8),
-              Text(
-                '대피로가 해결해드릴게요.',
-                style: DaepiroTextStyle.h5.copyWith(color: DaepiroColorStyle.g_900),
-              ),
+              headerWidget(),
               Spacer(),
-              Container(
-                width: double.infinity,
-                child: PrimaryFilledButton(
-                    onPressed: () {
-                      GoRouter.of(context).push('/onboarding/first');
-                    },
-                    backgroundColor: Color(0xFFFF6929),
-                    pressedColor:  Color(0xFFFF6929),
-                    child: Text(
-                      '다음',
-                      style: DaepiroTextStyle.body_1_b.copyWith(color: DaepiroColorStyle.white),
-                    ),
-                    verticalPadding: 12, borderRadius: 8),
-              ),
+              footerWidget(context),
               SizedBox(height: 16),
             ],
           ),
@@ -50,5 +29,41 @@ class OnboardingFirstScreen extends StatelessWidget {
       ),
     );
   }
+
+  Widget headerWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '반복되는 수신과 불명확한 재난문자로\n불편함을 겪으셨나요?',
+          style: DaepiroTextStyle.body_1_m.copyWith(color: DaepiroColorStyle.g_500),
+        ),
+        SizedBox(height: 8),
+        Text(
+          '대피로가 해결해드릴게요.',
+          style: DaepiroTextStyle.h5.copyWith(color: DaepiroColorStyle.g_900),
+        ),
+      ],
+    );
+  }
+
+  Widget footerWidget(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: PrimaryFilledButton(
+          onPressed: () {
+            GoRouter.of(context).push('/onboarding/first');
+          },
+          backgroundColor: Color(0xFFFF6929),
+          pressedColor:  Color(0xFFFF6929),
+          child: Text(
+            '다음',
+            style: DaepiroTextStyle.body_1_b.copyWith(color: DaepiroColorStyle.white),
+          ),
+          verticalPadding: 12, borderRadius: 8),
+    );
+  }
+
+  //TODO 일러스트 추가해야함
 
 }
