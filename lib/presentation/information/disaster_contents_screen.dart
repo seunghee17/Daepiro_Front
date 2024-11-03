ac42c88ca84f547fa073a517834980c6de40c004
Ojongseok <whdtjr6889@naver.com>
2024-11-03 12:37:14 +0900

    design: 재난 콘텐츠 - [행동요령 확인하기] 버튼 추가

--- lib/presentation/information/disaster_contents_screen.dart
+++ lib/presentation/information/disaster_contents_screen.dart
@@ -71,6 +71,31 @@ class DisasterContentsScreen extends StatelessWidget {
                               style: DaepiroTextStyle.body_1_m.copyWith(
                                 color: DaepiroColorStyle.g_300,
                               )
+                          ),
+                          const SizedBox(height: 20),
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 6, horizontal: 16),
+                            decoration: BoxDecoration(
+                              color: DaepiroColorStyle.g_600,
+                              borderRadius: BorderRadius.circular(99)
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  '행동요령 확인하기',
+                                  style: DaepiroTextStyle.body_2_m.copyWith(
+                                    color: DaepiroColorStyle.white,
+                                  )
+                                ),
+                                const SizedBox(width: 4),
+                                SvgPicture.asset(
+                                    'assets/icons/icon_arrow_right.svg',
+                                  colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                                )
+                              ],
+                            ),
                           )
                         ],
                       ),
