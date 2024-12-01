9c6be5b1f4157c6b69df2a7f99e4d1d76677459c
Ojongseok <whdtjr6889@naver.com>
2024-12-02 00:43:36 +0900

    chore: home 관련 API 연동 중

--- lib/presentation/home/main/home_view_model.dart
+++ lib/presentation/home/main/home_view_model.dart
@@ -1,18 +1,38 @@
+import 'package:daepiro/domain/usecase/home/home_status_usecase.dart';
 import 'package:daepiro/presentation/home/main/home_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// class HomeViewModel extends ChangeNotifier {
 final homeStateNotifierProvider = StateNotifierProvider<HomeViewModel, HomeState>((ref) {
-  return HomeViewModel();
+  return HomeViewModel(ref);
 });
 
 class HomeViewModel extends StateNotifier<HomeState> {
-  HomeViewModel() : super(HomeState());
+  HomeViewModel(this.ref) : super(HomeState()) {
+    getHomeStatus();
+  }
+
+  final StateNotifierProviderRef<HomeViewModel, HomeState> ref;
 
   int _selectedPopularPostCategory = 0;
   int get selectedPopularPostCategory => _selectedPopularPostCategory;
 
+  Future<void> getHomeStatus() async {
+    state = state.copyWith(isLoading: true);
+    try {
+      final response = await ref.read(
+          getHomeStatusUseCaseProvider(GetHomeStatusUseCase()).future
+      );
+      print(response.data?.isOccurred.toString());
 
+      state = state.copyWith(
+          isLoading: false,
+          isOccurred: response.data?.isOccurred ?? false
+      );
+    } catch (error) {
+      print('재난 발생상황 에러: $error');
+      state = state.copyWith(isLoading: false);
+    }
+  }
 
   void selectPopularPostCategory(int index) {
     _selectedPopularPostCategory = index;
