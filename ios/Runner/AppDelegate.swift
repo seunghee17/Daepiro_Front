72a2dd9e18a169cc9be709d579acfde6957fcbd0
Ojongseok <whdtjr6889@naver.com>
2025-01-31 16:15:27 +0900

    build: Flutter 버전 3.27.0 발생 이슈 대응

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import Flutter
 import UIKit
 import NaverThirdPartyLogin
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
