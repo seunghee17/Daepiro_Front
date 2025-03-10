150567301b8a62ba12384620abefdc5e99a4432a
seunghee17 <victoria9981@swu.ac.kr>
2025-03-10 19:37:09 +0900

    fix: ios 알람 중복 발생 오류 수정

--- lib/set_fcm.dart
+++ lib/set_fcm.dart
@@ -1,6 +1,3 @@
-
-import 'dart:io';
-
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
@@ -8,7 +5,8 @@ class SettingFCM {
   final _localNotifications = FlutterLocalNotificationsPlugin();
 
   @pragma('vm:entry-point')
-  static Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  static Future<void> firebaseMessagingBackgroundHandler(
+      RemoteMessage message) async {
     print('백그라운드 메시지 처리: ${message.notification?.title}');
   }
 
@@ -17,16 +15,16 @@ class SettingFCM {
     FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
 
     //로컬 알림 초기화
-    const androidSettings = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const androidSettings =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
     const iosSettings = DarwinInitializationSettings();
-    const settings = InitializationSettings(android: androidSettings, iOS: iosSettings);
+    const settings =
+        InitializationSettings(android: androidSettings, iOS: iosSettings);
 
-    await _localNotifications.initialize(
-        settings,
-      onDidReceiveNotificationResponse: (NotificationResponse response) {
-          print('알림 클릭!!!!!');
-      }
-    );
+    await _localNotifications.initialize(settings,
+        onDidReceiveNotificationResponse: (NotificationResponse response) {
+      print('알림 클릭!!!!!');
+    });
 
     const androidChannel = AndroidNotificationChannel(
       'high_importance_channel',
@@ -36,13 +34,14 @@ class SettingFCM {
     );
 
     final androidImplementation =
-    _localNotifications.resolvePlatformSpecificImplementation<
-        AndroidFlutterLocalNotificationsPlugin>();
+        _localNotifications.resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>();
     await androidImplementation?.createNotificationChannel(androidChannel);
 
-    // Foreground 알림 설정
-    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-      alert: true,
+    // ios Foreground 알림 설정
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: false,
       badge: true,
       sound: true,
     );
@@ -50,28 +49,30 @@ class SettingFCM {
     // Foreground 메시지 처리
     FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
       Map<String, dynamic> notification = message.data;
-      String title = notification.isEmpty ? message.notification!.title : notification['title'];
-      String body = notification.isEmpty ? message.notification!.body : notification['body'];
+      String title = notification.isEmpty
+          ? message.notification!.title
+          : notification['title'];
+      String body = notification.isEmpty
+          ? message.notification!.body
+          : notification['body'];
       print('알림 notification: $title');
       print('알림 notification: $body');
 
-      if(Platform.isAndroid) {
-        _localNotifications.show(
-          notification.hashCode,
-          title,
-          body,
-          const NotificationDetails(
-            android: AndroidNotificationDetails(
-              'high_importance_channel',
-              'High Importance Notifications',
-              importance: Importance.max,
-              priority: Priority.high,
-              icon: '@mipmap/ic_launcher',
-            ),
-            //iOS: DarwinNotificationDetails(sound: 'default'),
+      _localNotifications.show(
+        notification.hashCode,
+        title,
+        body,
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+            'high_importance_channel',
+            'High Importance Notifications',
+            importance: Importance.max,
+            priority: Priority.high,
+            icon: '@mipmap/ic_launcher',
           ),
-        );
-      }
-        });
+          iOS: DarwinNotificationDetails(sound: 'default'),
+        ),
+      );
+    });
   }
 }
